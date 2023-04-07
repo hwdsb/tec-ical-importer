b3d1fe78d35056feedd346fd33741887fc08c5a5
r-a-y <r-a-y@gmx.com>
2023-04-07 08:32:49 +0700

    Change user agent from Chrome to Safari when fetching iCalendars.
    PHP ICS Parser requires a user agent to be set to fetch Outlook
    iCalendars. Safari's version number doesn't change as often as Chrome,
    so let's use Safari's user agent instead.

--- includes/tec-ical-parser.php
+++ includes/tec-ical-parser.php
@@ -81,7 +81,7 @@ class TEC_iCal_Parser {
 				$parser = new ICal\ICal(
 					$ical['link'],
 					[
-						'httpUserAgent'  => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+						'httpUserAgent'  => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15',
 						'skipRecurrence' => true
 					]
 				);
